e1011ef83436c98efc37cfdf0a088b3dd31214d3
Nathan1006 <nrpetit@outlook.com>
2021-09-09 18:40:13 -0400

    Algorithm challenges

--- Intermediate21.js
+++ Intermediate21.js
@@ -1,17 +1,21 @@
 function orbitalPeriod(arr) {
-    var GM = 398600.4418
-    var earthRadius = 6367.4447
-    
-    for (let i in arr) {
-      var orbitsLengthPower3 = Math.pow((arr[i].avgAlt + earthRadius), 3)
-      
-      var rightHand = Math.pow((orbitsLengthPower3 / GM), 0.5)
-
-      var orbitalPer = Math.round(2 * Math.PI * rightHand)
-
-      arr[i] = {name: arr[i].name, orbitalPeriod: orbitalPer}
-    }
-    return arr;
-  }
+    var GM = 398600.4418;
+    var earthRadius = 6367.4447;
+    var a = 2 * Math.PI;
+    var myArr = [];
   
-  orbitalPeriod([{name : "sutnik", avgAlt : 35873.5553}]);
+    var getOrbPeriod = function(obj) {
+      var c = Math.pow(earthRadius + obj.avgAlt, 3);
+      var b = Math.sqrt(c / GM);
+      var orbPeriod = Math.round(a * b);
+      return {name: obj.name, orbitalPeriod: orbPeriod};
+    };
+  
+    for (var i in arr) {
+      myArr.push(getOrbPeriod(arr[i]));
+    }
+  
+    return myArr;
+}
+
+orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}])
